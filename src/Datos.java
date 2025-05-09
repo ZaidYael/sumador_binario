fc876ebca09be2bf3adee8f4b1b07f57a5a4c627
Zaid Tapia <zaid.tapia@apdevs.com>
2025-05-09 11:28:31 -0600

    maquina turing v2 solución al 100% corregida

--- src/Datos.java
+++ src/Datos.java
@@ -1,33 +1,53 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Datos {
+    Scanner sc = new Scanner(System.in);
     public String entrada, sumando1, sumando2;
-    private boolean comprobacion;
 
     public Datos(String entrada) {
         this.entrada = entrada;
     }
-    //comprueba si ambos sumandos son de la misma longitud
-    public void comprobarCadena(){
-        String aux =entrada;
-        if (aux.endsWith("=")) {
-            aux = aux.substring(0, aux.length() - 1);
-        }
-        String [] sumandos = aux.split("\\+");
-        sumando1=sumandos[0];
-        sumando2=sumandos[1];
-         if(comprobacion=sumando1.length()!=sumando2.length()){
-             System.out.println("Los numeros binarios son de distinta longitud");
-             System.exit(0);
-         }
-        System.out.println("Se ha comprobado que los numeros son de la misma longitud");
-         sonBinarios(sumando1,sumando2);
-    }
-    //comprueba si ambos sumandos son numeros binarios
-        public void sonBinarios(String sumando1, String sumando2){
-            if(!sumando1.matches("[0-1]*")||!sumando2.matches("[0-1]*")){
-                System.out.println("Uno o ambos sumandos NO son numeros binarios");
-                System.exit(0);
+
+    public void comprobarCadena() {
+        boolean entradaValida = false;
+
+        while (!entradaValida) {
+            try {
+                if (!entrada.contains("+") || !entrada.endsWith("=")) {
+                    System.out.println("La cadena debe contener '+' y terminar con '='.");
+                } else {
+                    String aux = entrada.substring(0, entrada.length() - 1);
+                    String[] sumandos = aux.split("\\+");
+
+                    if (sumandos.length != 2) {
+                        System.out.println("Formato incorrecto. Debe ser: numero1+numero2=");
+                    } else {
+                        sumando1 = sumandos[0];
+                        sumando2 = sumandos[1];
+                        if (sumando1.length() != sumando2.length()) {
+                            System.out.println("Los números binarios deben tener la misma longitud.");
+                        }
+                        else if (!sumando1.matches("[01]+") || !sumando2.matches("[01]+")) {
+                            System.out.println("Uno o ambos sumandos NO son números binarios.");
+                        } else {
+                            System.out.println("La cadena es válida.");
+                            entradaValida = true;
+                        }
+                    }
+                }
+
+                if (!entradaValida) {
+                    System.out.print("Ingresa de nuevo los números a sumar: ");
+                    entrada = sc.nextLine();
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Por favor escribe la cadena correctamente de la forma numero1+numero2=");
+                entrada = sc.nextLine();
             }
-            System.out.println("Se ha comprobado que los numeros son binarios");
         }
+    }
 }
+
--- src/Sumador.java
+++ src/Sumador.java
@@ -1,25 +1,33 @@
+import java.util.Stack;
+
 public class Sumador {
-    int pointer=0;
+    int pointer = 0;
     String carry;
     String entrada;
-    private void moverDerecha(){
+
+    private void moverDerecha() {
         pointer++;
     }
-    private void moverIzquierda(){
+
+    private void moverIzquierda() {
         pointer--;
     }
 
-    public Sumador(String entrada){
-        this.entrada=entrada;
+    public Sumador(String entrada) {
+        this.entrada = entrada;
     }
-    public void q0(){
-        while (entrada.charAt(pointer) != '+'){
+
+    public void q0() {
+        System.out.println("qo");
+        while (entrada.charAt(pointer) != '+') {
             pointer++;
         }
+        System.out.println("entrada" + entrada);
         q1();
     }
-    public void q1(){
 
+    public void q1() {
+        System.out.println("q1");
 
         try {
             do {
@@ -33,11 +41,13 @@ public class Sumador {
                 temp = temp + entrada.charAt(count);
             }
             temp = temp + "_";
-            for (count = pointer +1; count < entrada.length(); count++) {
+            for (count = pointer + 1; count < entrada.length(); count++) {
                 temp = temp + entrada.charAt(count);
             }
             entrada = temp;
-            switch (aux){
+            System.out.println(pointer);
+            System.out.println("entrada" + entrada);
+            switch (aux) {
                 case '1':
                     q2();
                     break;
@@ -46,36 +56,36 @@ public class Sumador {
                     break;
 
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             pointer++;
             q11();
         }
     }
 
-    public void q2(){
-
+    public void q2() {
+        System.out.println("q2");
 
         do {
             pointer++;
-        }while (entrada.charAt(pointer) != '=');
+        } while (entrada.charAt(pointer) != '=');
         q4();
     }
 
-    public void q3(){
-
+    public void q3() {
+        System.out.println("q3");
         do {
             pointer++;
-        }while (entrada.charAt(pointer) != '=');
+        } while (entrada.charAt(pointer) != '=');
 
         q5();
     }
 
-    public void q4(){
-
+    public void q4() {
+        System.out.println("q4");
 
         do {
             pointer--;
-        }while (entrada.charAt(pointer) != '1' && entrada.charAt(pointer)!='0');
+        } while (entrada.charAt(pointer) != '1' && entrada.charAt(pointer) != '0');
         char aux = entrada.charAt(pointer);
         String temp = "";
         int count = 0;
@@ -83,10 +93,11 @@ public class Sumador {
             temp = temp + entrada.charAt(count);
         }
         temp = temp + "_";
-        for (count = pointer +1; count < entrada.length(); count++) {
+        for (count = pointer + 1; count < entrada.length(); count++) {
             temp = temp + entrada.charAt(count);
         }
         entrada = temp;
+        System.out.println(entrada);
         switch (aux) {
             case '1':
                 q12();
@@ -97,11 +108,11 @@ public class Sumador {
         }
     }
 
-    public void q5(){
-
+    public void q5() {
+        System.out.println("q5");
         do {
             pointer--;
-        }while (entrada.charAt(pointer) != '1' && entrada.charAt(pointer)!='0');
+        } while (entrada.charAt(pointer) != '1' && entrada.charAt(pointer) != '0');
         char aux = entrada.charAt(pointer);
         String temp = "";
         int count = 0;
@@ -109,11 +120,12 @@ public class Sumador {
             temp = temp + entrada.charAt(count);
         }
         temp = temp + "_";
-        for (count = pointer +1; count < entrada.length(); count++) {
+        for (count = pointer + 1; count < entrada.length(); count++) {
             temp = temp + entrada.charAt(count);
         }
         entrada = temp;
-        switch (aux){
+        System.out.println("entrada" + entrada);
+        switch (aux) {
             case '1':
                 q6();
                 break;
@@ -123,66 +135,80 @@ public class Sumador {
         }
     }
 
-    public void q6(){
+    public void q6() {
         System.out.println("q6");
         System.out.println(entrada);
         do {
             pointer++;
-        }while (pointer != (entrada.length() - 1)&& entrada.charAt(pointer) != 'z');
+        } while (pointer != (entrada.length() - 1) && entrada.charAt(pointer) != 'z');
 
-        if (entrada.charAt(pointer) == 'z'){
+        if (entrada.charAt(pointer) == 'z') {
             entrada = entrada.replace('z', '0');
+            System.out.println(entrada);
             q8();
         } else {
             entrada += "1";
+            System.out.println(entrada);
             q9();
         }
     }
-    public void q7(){
 
+    public void q7() {
+        System.out.println("q7");
 
         String temp = "";
         do {
             pointer++;
-        }while (pointer!= entrada.length());
-        entrada += "0";
+        } while (pointer != entrada.length());
+        if (entrada.contains("z")) {
+            entrada = entrada.replace('z', '1');
+        } else {
+            entrada += "0";
+        }
+        System.out.println("entrada" + entrada);
         q9();
     }
-    public void q8(){
 
-
-        entrada+="z";
+    public void q8() {
+        System.out.println("q8");
+        entrada += "z";
+        System.out.println("entrada" + entrada);
         q9();
 
     }
-    public void q9(){
+
+    public void q9() {
 
 
-        do{
+        do {
             pointer--;
-        } while (pointer>0);
+        } while (pointer > 0);
         q0();
     }
 
-    public void q11(){
-
+    public void q11() {
+        System.out.println("q11");
 
         do {
             pointer++;
-        }while (entrada.charAt(pointer) != 'z' && pointer!= (entrada.length() - 1));
-        entrada = entrada.replace('z','1');
-        System.out.println(entrada);
+        } while (entrada.charAt(pointer) != 'z' && pointer != (entrada.length() - 1));
+        entrada = entrada.replace('z', '1');
+        System.out.println("entrada" + entrada);
     }
 
-    public void q12(){
+    public void q12() {
+        System.out.println("q12");
         do {
             pointer++;
-        }while (entrada.charAt(pointer) != 'z' && pointer != (entrada.length() - 1));
-        if ( entrada.contains("z")){
-            entrada = entrada.replace('z','1');
-        }else {
+        } while (entrada.charAt(pointer) != 'z' && pointer != (entrada.length() - 1));
+        if (entrada.contains("z")) {
+            entrada = entrada.replace('z', '1');
+            System.out.println("entrada" + entrada);
+        } else {
             entrada += "0";
+            System.out.println("entrada" + entrada);
         }
         q8();
     }
+
 }
